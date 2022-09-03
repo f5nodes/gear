66e8580772c68d33e42d87759a1671dbf17efb65
vasylcode <vasylcode@gmail.com>
2022-09-03 14:13:09 +0200

    error info added

--- update.sh
+++ update.sh
@@ -5,13 +5,21 @@ rm gear-nightly-linux-x86_64.tar.xz
 $HOME/gear-node purge-chain -y
 sudo systemctl restart gear
 if [ "$language" = "uk" ]; then
-    echo -e "\n\e[93mGear Node Updated!\e[0m\n"
-    echo -e "Подивитись логи ноди \e[92mjournalctl -n 100 -f -u gear\e[0m"
-    echo -e "\e[92mCTRL + C\e[0m щоб вийти з логів\n"
-    echo -e "Зробіть бекап \e[92m$HOME/.local/share/gear-node/chains/staging_testnet_v2/network\e[0m"
+    if [[ `service gear-node status | grep active` =~ "running" ]]; then
+        echo -e "\n\e[93mGear Node Updated!\e[0m\n"
+        echo -e "Подивитись логи ноди \e[92mjournalctl -n 100 -f -u gear\e[0m"
+        echo -e "\e[92mCTRL + C\e[0m щоб вийти з логів\n"
+        echo -e "Зробіть бекап \e[92m$HOME/.local/share/gear-node/chains/staging_testnet_v2/network\e[0m"
+    else
+        echo -e "\e[91mПомилка\e[39m під час оновлення Gear ноди."
+    fi
 else
-    echo -e "\n\e[93mGear Node Updated!\e[0m\n"
-    echo -e "Check node logs \e[92mjournalctl -n 100 -f -u gear\e[0m"
-    echo -e "\e[92mCTRL + C\e[0m to exit logs\n"
-    echo -e "Backup \e[92m$HOME/.local/share/gear-node/chains/staging_testnet_v2/network\e[0m"
+    if [[ `service gear-node status | grep active` =~ "running" ]]; then
+        echo -e "\n\e[93mGear Node Updated!\e[0m\n"
+        echo -e "Check node logs \e[92mjournalctl -n 100 -f -u gear\e[0m"
+        echo -e "\e[92mCTRL + C\e[0m to exit logs\n"
+        echo -e "Backup \e[92m$HOME/.local/share/gear-node/chains/staging_testnet_v2/network\e[0m"
+    else
+        echo -e "\e[91mError\e[39m while updating Gear node."
+    fi
 fi
