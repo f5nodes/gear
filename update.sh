b10bfad09d8773c69c54d352d8e5fc4c3668f023
vasylcode <vasylcode@gmail.com>
2023-03-24 00:44:35 +0100

    v7 update

--- update.sh
+++ update.sh
@@ -16,7 +16,7 @@ if [ "$language" = "uk" ]; then
         echo -e "\n\e[93mGear Node Updated!\e[0m\n"
         echo -e "Подивитись логи ноди \e[92mjournalctl -n 100 -f -u gear\e[0m"
         echo -e "\e[92mCTRL + C\e[0m щоб вийти з логів\n"
-        echo -e "Зробіть бекап \e[92m$HOME/.local/share/gear/chains/gear_staging_testnet_v5/network\e[0m"
+        echo -e "Зробіть бекап \e[92m$HOME/.local/share/gear/chains/gear_staging_testnet_v7/network\e[0m"
     else
         echo -e "\e[91mПомилка\e[39m під час оновлення Gear ноди."
     fi
@@ -25,7 +25,7 @@ else
         echo -e "\n\e[93mGear Node Updated!\e[0m\n"
         echo -e "Check node logs \e[92mjournalctl -n 100 -f -u gear\e[0m"
         echo -e "\e[92mCTRL + C\e[0m to exit logs\n"
-        echo -e "Backup \e[92m$HOME/.local/share/gear/chains/gear_staging_testnet_v5/network\e[0m"
+        echo -e "Backup \e[92m$HOME/.local/share/gear/chains/gear_staging_testnet_v7/network\e[0m"
     else
         echo -e "\e[91mError\e[39m while updating Gear node."
     fi
